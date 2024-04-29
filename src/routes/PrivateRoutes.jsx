4ceaa907c9c4418eec18b8cd399579d39026da57
tahmedofficial <tanvirofcuo@gmail.com>
2024-04-30 01:03:04 +0600

    fixed problem

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -16,7 +16,7 @@ const PrivateRoutes = ({ children }) => {
         return children
     }
 
-    return (<Navigate state={location.pathname} to="/login"></Navigate>);
+    return <Navigate state={location.pathname} to="/login"></Navigate>
 };
 
 export default PrivateRoutes;
